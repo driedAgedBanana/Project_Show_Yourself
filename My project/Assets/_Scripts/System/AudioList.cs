765a399709513076c41116955503b163ef53a023
dryAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2026-02-25 20:11:25 +0100

    Try to add SFX

--- My project/Assets/_Scripts/System/AudioList.cs
+++ My project/Assets/_Scripts/System/AudioList.cs
@@ -4,6 +4,13 @@ public class AudioList : MonoBehaviour
 {
     public AudioSource source;
 
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.playOnAwake = false;
+        source.loop = false;
+    }
+
     // This function will show up in your Animation Event dropdown
     public void PlaySound(AudioClip clip)
     {
